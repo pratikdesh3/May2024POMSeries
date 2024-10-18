062b8c8881c8707155025e4a30860cd326b913d6
pratikdesh3 <54172809+pratikdesh3@users.noreply.github.com>
2024-10-18 16:20:01 +0530

    Create TestPage.java

---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class TestPage {
+
+	int i = 10;
+	
+	By loc = By.id("...test");
+	
+	public void getPage() {
+		System.out.println("click on test");
+	}
+
+}
